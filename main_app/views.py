ad59414dde7cfa412e6bfed159c5a871f8df341e
Sammark1 <samaustinmark@gmail.com>
2022-04-18 16:40:58 -0400

    initial paths and views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+class Landing(View):
+    def get(self, request):
+        return HttpResponse("Landing")
