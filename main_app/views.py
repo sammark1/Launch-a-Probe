2b4c1f7d890a0adcd0b57b9179e085cd722aa9a3
Sammark1 <samaustinmark@gmail.com>
2022-04-18 17:04:30 -0400

    add templates structure

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
-class Landing(View):
-    def get(self, request):
-        return HttpResponse("Landing")
+class Landing(TemplateView):
+    template_name = "landing.html"
